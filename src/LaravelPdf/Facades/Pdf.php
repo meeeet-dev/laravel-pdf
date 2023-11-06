c08f89645f2a6e10c0f72f99a57f4a86482bd4cb
Meet Bhanabhagwan <meetbhana9567@gmail.com>
2023-11-06 19:19:29 +0530

    initial commit

---
+++ src/LaravelPdf/Facades/Pdf.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace MeeeetDev\LaravelPdf\Facades;
+
+use Illuminate\Support\Facades\Facade as BaseFacade;
+
+class Pdf extends BaseFacade {
+
+	/**
+	 * Get the registered name of the component.
+	 *
+	 * @return string
+	 */
+	protected static function getFacadeAccessor() { return 'mpdf.wrapper'; }
+}
